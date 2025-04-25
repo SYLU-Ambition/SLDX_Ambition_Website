f61eb7c4a70054273e9328c1e1f29267309bfc00
战空 <2276539496@qq.com>
2025-04-25 14:51:03 +0800

    结构变更

--- .vitepress/nav.mjs
+++ .vitepress/nav.mjs
@@ -7,15 +7,28 @@ export default [//页眉按钮
             { text: '战队历史', link: '/Ambition_Introduction/team_history' },
             { text: '战队合照', link: '/Ambition_Introduction/team_photos' },
             { text: '战队历代成员', link: '/Ambition_Introduction/team_members' },
-            { text: '宣传视频', link: '/Ambition_Introduction/promotional_video' } 
+            { text: '宣传视频', link: '/Ambition_Introduction/promotional_video' }
         ]
 
     },
     {
-        text: '加入我们', items: [
+        text: '加入我们',
+        items: [
             { text: '组别介绍', link: '/Sign_Up/group_introduction' },
             { text: '招新通道', link: '/Sign_Up/sign_up' },
             { text: 'Q&A', link: '/Sign_Up/QA' }
         ]
-    }
+    },
+    {
+        text: '战队历史',
+        items: [
+            { text: '战队历史', link: '/History/history1' },
+        ]
+    },
+    {
+        text: '照片展示',
+        items: [
+            { text: '24联盟赛', link: '/Photo/24LMS' },
+        ]
+    },
 ]
--- .vitepress/sidebar.mjs
+++ .vitepress/sidebar.mjs
@@ -1,10 +1,9 @@
 export default [//左侧边栏
     {
         text: '战队介绍',
-        collapsed: true,
+        collapsed: false,
         items: [
             { text: '战队简介', link: '/Ambition_Introduction/team_introduction' },
-            { text: '战队历史', link: '/Ambition_Introduction/team_history' },
             { text: '战队合照', link: '/Ambition_Introduction/team_photos' },
             { text: '战队历代成员', link: '/Ambition_Introduction/team_members' },
             { text: '宣传视频', link: '/Ambition_Introduction/promotional_video' } 
@@ -12,11 +11,25 @@ export default [//左侧边栏
     },
     {
         text: '加入我们',
-        collapsed: true,
+        collapsed: false,
         items: [
             { text: '组别介绍', link: '/Sign_Up/group_introduction' },
             { text: '招新通道', link: '/Sign_Up/sign_up' },
             { text: 'Q&A', link: '/Sign_Up/QA' }
         ]
-    }
+    },
+    {
+        text: '战队历史',
+        collapsed: true,
+        items: [
+            { text: '战队历史', link: '/History/history1' },
+        ]
+    },
+    {
+        text: '照片展示',
+        collapsed: true,
+        items: [
+            { text: '24联盟赛', link: '/Photo/24LMS' },
+        ]
+    },
 ]
