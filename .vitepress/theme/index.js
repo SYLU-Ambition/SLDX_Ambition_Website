d2e3f7627c3e0ed68fd2fc9e4c1d4de273f13f52
Zhan_Kong <ldzhankong@foxmail.com>
2025-05-29 14:17:15 +0800

    简化

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,22 +1,24 @@
 import { h } from 'vue'
-import DefaultTheme from 'vitepress/theme' 
 import mediumZoom from 'medium-zoom'
 import { onMounted, watch, nextTick } from 'vue'
 import { useRoute } from 'vitepress'
-import './style.css' 
+import './style.css'
 import escookTheme from '@escook/vitepress-theme'
-import '@escook/vitepress-theme/style.css' 
-import MyLayout from './MyLayout.vue' 
-import NCard from './NCard.vue' 
+import '@escook/vitepress-theme/style.css'
+import MyLayout from './MyLayout.vue'
+import NCard from './NCard.vue'
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: escookTheme,
-  Layout: MyLayout, 
+  Layout: MyLayout,
   setup() {
     const route = useRoute()
     const initZoom = () => {
-      mediumZoom(".main img", { background: "var(--vp-c-bg)" })
+      // 确保只在浏览器环境执行
+      if (typeof window !== 'undefined') {
+        mediumZoom(".main img", { background: "var(--vp-c-bg)" })
+      }
     }
 
     onMounted(() => {
@@ -33,18 +35,5 @@ export default {
   enhanceApp({ app, router, siteData }) {
     // --- 注册全局组件 ---
     app.component('NCard', NCard) // 注册 NCard 组件
-
-    // --- 保持百度统计逻辑 ---
-    if (import.meta.env.PROD) {
-      router.onBeforeRouteChange = (to) => {
-        if (typeof _hmt !== 'undefined' && !!to) {
-          try {
-            _hmt.push(['_trackPageview', to])
-          } catch (e) {
-            console.error('Baidu Analytics Error:', e)
-          }
-        }
-      }
-    }
   }
 }
